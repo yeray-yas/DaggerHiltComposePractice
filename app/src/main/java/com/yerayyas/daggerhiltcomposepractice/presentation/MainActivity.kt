ce5bc7876923540f94cc2c1ca94595049a79572b
yeray-yas <yerayarucas@gmail.com>
2023-07-17 19:57:10 +0100

    Rest of code. MainActivity code wasn't added yet

--- app/src/main/java/com/yerayyas/daggerhiltcomposepractice/presentation/MainActivity.kt
+++ app/src/main/java/com/yerayyas/daggerhiltcomposepractice/presentation/MainActivity.kt
@@ -8,8 +8,16 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.yerayyas.daggerhiltcomposepractice.presentation.pokemon_list.PokemonListScreen
 import com.yerayyas.daggerhiltcomposepractice.presentation.ui.theme.DaggerHiltComposePracticeTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,30 +27,35 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DaggerHiltComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = "pokemon_list_screen"
                 ) {
-                    Greeting("Android")
+                    composable("pokemon_list_screen") {
+                        PokemonListScreen(navController = navController)
+                    }
+                    composable(
+                        "pokemon_detail_screen/{dominantColor}{pokemonName}/",
+                        arguments = listOf(
+                            navArgument("dominantColor") {
+                                type = NavType.IntType
+                            },
+                            navArgument("pokemonName") {
+                                type = NavType.StringType
+                            }
+                        )
+                    ) {
+                        val dominantColor = remember {
+                            val color = it.arguments?.getInt("dominantColor")
+                            color?.let { Color(it) } ?: Color.White
+                        }
+                        val pokemonName = remember {
+                            it.arguments?.getString("pokemonName")
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DaggerHiltComposePracticeTheme {
-        Greeting("Android")
     }
 }
